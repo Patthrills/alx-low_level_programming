4a6034c9d6dd8a8f6a22a3dddc37e0a763a7ec89
patthrills <asubiaropatrick@gmail.com>
2022-12-12 19:07:56 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -21,10 +21,14 @@ int main(void)
 		char low;
 
 
-
 		for (low = 'a'; low <= 'z'; low++)
-			putchar(low);
-			printf("\n");
 
-		return (0);
+						putchar(low);
+
+					printf("\n");
+
+
+
+						return (0);
+
 }
