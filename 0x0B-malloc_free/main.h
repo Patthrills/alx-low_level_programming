4fa699792741aaeaeeff69b0354d8a458fe1b331
patthrills <asubiaropatrick@gmail.com>
2023-11-22 20:58:39 -0800

    malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -5,3 +12,6 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+# endif
